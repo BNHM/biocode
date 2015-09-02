db831b69effad6052b54f50ad1cf5677d8133d50
John Deck <jdeck88@gmail.com>
2015-09-02 15:42:35 -0700

    adjusting backup scripts, fixing a bug where the wrong variable being used, adding compression for faster transfer from gall, and finally, gzipping result to save disk space

--- scripts/cron/biocode_db_backup.sh
+++ scripts/cron/biocode_db_backup.sh
@@ -3,6 +3,8 @@ user=$(grep g_db_fulluser /usr/local/web/biocode/cgi/biocode_settings | awk -F\"
 pass=$(grep g_db_fullpass /usr/local/web/biocode/cgi/biocode_settings | awk -F\" '{print $(NF-1)}')
 location=$(grep g_db_location /usr/local/web/biocode/cgi/biocode_settings | awk -F\" '{print $(NF-1)}')
 
-/usr/bin/mysqldump -p$pass -u$user -h $host geneious > /data/biocode/database_backups/geneious_db.sql
-/usr/bin/mysqldump -p$pass -u$user -h $location biocode > /data/biocode/database_backups/biocode_db.sql
+/usr/bin/mysqldump -p$pass -u$user -h $location --compress geneious > /data/biocode/database_backups/geneious_db.sql
+/bin/gzip -f /data/biocode/database_backups/geneious_db.sql
 
+/usr/bin/mysqldump -p$pass -u$user -h $location --compress biocode > /data/biocode/database_backups/biocode_db.sql
+/bin/gzip -f /data/biocode/database_backups/biocode_db.sql
--- scripts/cron/biocode_labbench_backup.sh
+++ scripts/cron/biocode_labbench_backup.sh
@@ -3,5 +3,6 @@ user=$(grep g_db_fulluser /usr/local/web/biocode/cgi/biocode_settings | awk -F\"
 pass=$(grep g_db_fullpass /usr/local/web/biocode/cgi/biocode_settings | awk -F\" '{print $(NF-1)}')
 location=$(grep g_db_location /usr/local/web/biocode/cgi/biocode_settings | awk -F\" '{print $(NF-1)}')
 
-/usr/bin/mysqldump -p$pass -u$user -h $host labbench > /data/biocode/database_backups/labbench_db.sql
+/usr/bin/mysqldump -p$pass -u$user -h $location --compress labbench > /data/biocode/database_backups/labbench_db.sql
+/bin/gzip -f /data/biocode/database_backups/labbench_db.sql
 
